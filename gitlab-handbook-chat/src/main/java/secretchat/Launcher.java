71cfef2e65597531fe6ab9464ec4d6b489ea1a20
phuongnha <danhphha@gmail.com>
2026-06-09 11:52:39 +0700

    user-service

--- gitlab-handbook-chat/src/main/java/secretchat/Launcher.java
+++
@@ -1,9 +0,0 @@
-package secretchat;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(ChatApplication.class, args);
-    }
-}
