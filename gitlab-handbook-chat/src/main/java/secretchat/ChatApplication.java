b8d016e67e5b73bbd717476e506cc3190cc22289
dpn <22130191@st.hcmuaf.edu.vn>
2026-06-25 18:37:30 +0700

    fix: update application title to remove redundant 'Login' suffix

--- gitlab-handbook-chat/src/main/java/secretchat/ChatApplication.java
+++ gitlab-handbook-chat/src/main/java/secretchat/ChatApplication.java
@@ -12,7 +12,7 @@ public class ChatApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ChatApplication.class.getResource("/fxml/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("The GitLab Handbook Chat - Login");
+        stage.setTitle("The GitLab Handbook Chat");
         stage.setScene(scene);
         stage.show();
     }
